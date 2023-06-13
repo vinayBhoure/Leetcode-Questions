abacce12f59c7fb015c05f7a546a9d7b124a5e31
Vinay Bhoure <92016717+vinayBhoure@users.noreply.github.com>
2023-06-13 20:24:32 +0530

    Time: 6 ms (23.15%), Space: 8.3 MB (70.58%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -21,8 +21,25 @@ public:
         preorder(root->right,ans);
     }
     vector<int> preorderTraversal(TreeNode* root) {
+   /*
         vector<int> ans;
         preorder(root, ans);
         return ans; 
+    */
+        vector<int> ans;
+        stack<TreeNode*> st;
+        
+        if(root == NULL) return ans;
+        
+        st.push(root);
+        while(!st.empty())
+        {
+            root = st.top();
+            st.pop();
+            ans.push_back(root->val);
+            if(root->right != NULL) st.push(root->right);
+            if(root->left != NULL) st.push(root->left);
+        }
+        return ans;
     }
 };
