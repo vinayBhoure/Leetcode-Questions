d2a6586111c220762e302578f3f3ab0c37e3c120
Vinay Bhoure <92016717+vinayBhoure@users.noreply.github.com>
2023-07-01 12:36:13 +0530

    Time: 19 ms (5.24%), Space: 16.3 MB (6.19%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,7 +1,87 @@
+class TrieNode{
+public:
+    char ch;
+    TrieNode* children[26];
+    int countchild;
+    bool isterminal;
+    
+    TrieNode(char ch){
+        this->ch = ch;
+        for(int i=0; i<26; i++)
+        {
+            children[i] = NULL;
+        }
+        countchild = 0;
+        isterminal = false;
+    }
+};
+
+class Trie{
+public:
+    TrieNode* root;
+    
+    Trie(char ch){
+        root = new TrieNode(ch);
+    }
+    
+    void insertutil(TrieNode* root, string word)
+    {
+       //base case
+        if(word.length() == 0)
+        {
+            root->isterminal = true;
+            return;
+        }
+        
+        TrieNode* child;
+        int i = word[0]-'a';
+        
+        if(root->children[i] != NULL)
+        {
+            child = root->children[i];
+        }
+        else{
+            child = new TrieNode(word[0]);
+            root->countchild++;
+            root->children[i] = child;
+        }
+        
+        insertutil(child,word.substr(1));
+    }
+    
+    void insertword(string word)
+    {
+        insertutil(root,word);
+    }
+    
+    void lcp(string str, string &ans)
+    {
+       for(int i=0; i<str.length(); i++)
+       {
+           char ch = str[i];
+          if(root->countchild == 1)
+          {
+              ans = ans + ch;
+              
+              int i = ch - 'a';
+              root = root->children[i];
+          }
+           else{
+               break;
+           }
+           
+           if(root->isterminal)
+           {
+               break;
+           }
+       }
+    }
+};
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-     
+     /*
         string ans  = "";
         sort(strs.begin(), strs.end());
         string a = strs[0];
@@ -17,6 +97,24 @@ public:
         }
         
         return ans;
+        */
+        /////////////////////////    USING TRIES
         
+       
+        
+        Trie *t = new Trie('\0');
+        
+        for(int i=0; i< strs.size(); i++)
+        {
+            if(!strs[i].size()) return "";
+            
+            t->insertword(strs[i]);
+        }
+        
+        string first = strs[0];
+        string ans = "";
+        
+        t->lcp(first, ans);
+        return ans;
     }
 };
